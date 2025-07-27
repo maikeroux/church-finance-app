c78cd0ce5198d4200235962038f121bf7ac95b4c
jdelacruz19-seneca <jdelacruz19@myseneca.ca>
2025-07-26 22:23:59 -0400

    fixed typo for authcontroller

--- user-service/src/controllers/authController.js
+++ user-service/src/controllers/authController.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../models/user');
 const logEvent = require('../utils/logEvent');
 
 // const { hashPassword, comparePassword } = require('../utils/hash');
